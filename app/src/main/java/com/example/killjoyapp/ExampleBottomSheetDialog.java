d5b04bbaabea07a5fe537a21e093970f01e3492c
Destiny-02 <dli393@aucklanduni.ac.nz>
2021-04-18 23:11:40 +1200

    delete redundant imports

--- app/src/main/java/com/example/killjoyapp/ExampleBottomSheetDialog.java
+++ app/src/main/java/com/example/killjoyapp/ExampleBottomSheetDialog.java
@@ -1,15 +1,11 @@
 package com.example.killjoyapp;
 
-import android.content.Context;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.example.killjoyapp.R;
 
 
 public class ExampleBottomSheetDialog extends BottomSheetDialogFragment {
--- app/src/main/java/com/example/killjoyapp/MainActivity.java
+++ app/src/main/java/com/example/killjoyapp/MainActivity.java
@@ -1,19 +1,10 @@
 package com.example.killjoyapp;
 
 import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
-
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Button;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
