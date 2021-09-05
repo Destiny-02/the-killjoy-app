449f4142825292bab16f03faf2dfb13520209ef0
Destiny <destinylinz@gmail.com>
2021-09-05 12:09:12 +1200

    add placeholders for other maps

--- app/src/main/java/com/example/killjoyapp/IceboxFragment.java
+++ app/src/main/java/com/example/killjoyapp/IceboxFragment.java
@@ -8,18 +8,46 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
 
 public class IceboxFragment extends Fragment {
+
+    RecyclerView recyclerView;
+    MyAdapter myAdapter;
 
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
+        View v = inflater.inflate(R.layout.fragment_icebox, container, false);
+        recyclerView = v.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        myAdapter = new MyAdapter(getContext(), getMyList());
+        recyclerView.setAdapter(myAdapter);
+
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Icebox");
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_icebox, container, false);
+        return v;
+    }
+
+    private ArrayList<Model> getMyList() {
+
+        ArrayList<Model> models = new ArrayList<>();
+
+        Model m = new Model();
+
+        m.setTitle("Title");
+        m.setDescription("* Under construction *");
+        m.setImg(R.mipmap.ic_launcher);
+        m.setUrl("");
+        models.add(m);
+
+        return models;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
