6491bf9200e714bec14e5a97b0832faebb64f20f
Destiny <destinylinz@gmail.com>
2021-09-05 14:16:44 +1200

    remove videoview

--- app/src/main/java/com/example/killjoyapp/MyHolder.java
+++ app/src/main/java/com/example/killjoyapp/MyHolder.java
@@ -1,21 +1,19 @@
 package com.example.killjoyapp;
 
 import android.view.View;
-import android.webkit.WebView;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView;
 
 public class MyHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
     ImageView imageView;
     TextView title, des;
     ItemClickListener itemClickListener;
-    YouTubePlayerView videoView;
+    TextView video;
 
     MyHolder(@NonNull View itemView) {
         super(itemView);
@@ -23,7 +21,7 @@ public class MyHolder extends RecyclerView.ViewHolder implements View.OnClickLis
         this.imageView = itemView.findViewById(R.id.image_preview_card);
         this.title = itemView.findViewById(R.id.title_card);
         this.des = itemView.findViewById(R.id.description_card);
-        this.videoView = itemView.findViewById(R.id.video_web_view_card);
+        this.video = itemView.findViewById(R.id.video_card);
 
         itemView.setOnClickListener(this);
 
