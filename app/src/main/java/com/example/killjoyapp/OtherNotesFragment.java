c3b560daee8c78af1552fa381a5d14987f829524
Destiny <destinylinz@gmail.com>
2021-09-05 16:22:07 +1200

    complete other notes page

--- app/src/main/java/com/example/killjoyapp/OtherNotesFragment.java
+++ app/src/main/java/com/example/killjoyapp/OtherNotesFragment.java
@@ -1,9 +1,11 @@
 package com.example.killjoyapp;
 
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,10 +18,19 @@ public class OtherNotesFragment extends Fragment {
             LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState
     ) {
-
+        View v = inflater.inflate(R.layout.fragment_other_notes, container, false);
         ((AppCompatActivity)getActivity()).getSupportActionBar().setTitle("Other Notes");
+
+        TextView description4 = (TextView) v.findViewById(R.id.description4);
+        description4.setText(R.string.sources_text);
+        description4.setMovementMethod(LinkMovementMethod.getInstance());
+
+        TextView description2 = (TextView) v.findViewById(R.id.description2);
+        description2.setText(R.string.about_author_text);
+        description2.setMovementMethod(LinkMovementMethod.getInstance());
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_other_notes, container, false);
+        return v;
     }
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
